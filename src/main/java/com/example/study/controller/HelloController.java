c793747d7920136043e75338f9be3be447e406b6
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-16 23:00:38 +0900

    refactor: 패키지변경

--- src/main/java/com/example/study/controller/HelloController.java
+++ src/main/java/com/example/study/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.jwt.controller;
+package com.example.study.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
