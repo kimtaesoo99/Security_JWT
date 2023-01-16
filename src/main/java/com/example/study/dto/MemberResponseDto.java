58d3e7144834ef04561ce91e6f452df502ef6ec2
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-16 23:52:36 +0900

    feat: 멤버 response DTO 추가

---
+++ src/main/java/com/example/study/dto/MemberResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.study.dto;
+
+import com.example.study.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberResponseDto {
+    private String email;
+
+    public static MemberResponseDto of(Member member) {
+        return new MemberResponseDto(member.getEmail());
+    }
+}
