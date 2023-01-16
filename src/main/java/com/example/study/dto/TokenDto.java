35c92b88ae31d6538382fab3f3500d13c2598b7a
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-16 23:55:16 +0900

    feat: 토큰 DTO 추가

---
+++ src/main/java/com/example/study/dto/TokenDto.java
@@ -0,0 +1,16 @@
+package com.example.study.dto;
+
+import lombok.*;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenDto {
+
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+    private Long accessTokenExpiresIn;
+}
