8fad8fda7e5991b3b6098ea127dd916b98b45be3
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-16 23:52:07 +0900

    feat: 멤버 레포 추가

---
+++ src/main/java/com/example/study/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.example.study.repository;
+
+import com.example.study.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
