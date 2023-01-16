b30951c1cb55f8409b6184c6d6545ef2b7fc7f80
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-16 23:51:37 +0900

    refactor: 멤버를 단순하게 수정

--- src/main/java/com/example/study/entity/Member.java
+++ src/main/java/com/example/study/entity/Member.java
@@ -11,20 +11,22 @@ import lombok.*;
 @NoArgsConstructor
 public class Member {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
 
-    @Column(name = "name", length = 50)
-    private String name;
+    private String email;
 
-    @Column(name = "password", length = 100)
     private String password;
-
-    @Column(name = "nickname", length = 50)
-    private String nickname;
-
 
     @Enumerated(EnumType.STRING)
     private Authority authority;
+
+    @Builder
+    public Member(String email, String password, Authority authority) {
+        this.email = email;
+        this.password = password;
+        this.authority = authority;
+    }
 }
