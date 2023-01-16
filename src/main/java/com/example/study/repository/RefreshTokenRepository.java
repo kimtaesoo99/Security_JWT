d173aa87c7953a1c84543b5aa94b0ac83bad9d1a
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-16 23:53:11 +0900

    feat: 리플래쉬 토큰 레포 추가

---
+++ src/main/java/com/example/study/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.example.study.repository;
+
+import com.example.study.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByKey(String key);
+}
