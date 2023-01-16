bb62fa66a43cc38cebbcc39eb0026d8a0b378015
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-16 23:53:38 +0900

    refactor: corsFilter를 타도록 수정

--- src/main/java/com/example/study/config/SecurityConfig.java
+++ src/main/java/com/example/study/config/SecurityConfig.java
@@ -5,20 +5,22 @@ import com.example.study.jwt.JwtAuthenticationEntryPoint;
 import com.example.study.jwt.JwtSecurityConfig;
 import com.example.study.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final TokenProvider tokenProvider;
+    private final CorsFilter corsFilter;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
@@ -27,17 +29,13 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    // h2 database 테스트가 원활하도록 관련 API 들은 전부 무시
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-            .requestMatchers("/h2-console/**", "/favicon.ico");
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // CSRF 설정 Disable
         http.csrf().disable()
+
+            .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
 
             // exception handling 할 때 우리가 만든 클래스를 추가
             .exceptionHandling()
