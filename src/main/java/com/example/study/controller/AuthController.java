6a38e5b526e3f7192e166cf9894e8260b9d625e7
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-16 23:48:55 +0900

    refactor: auth 컨트롤러 추가

---
+++ src/main/java/com/example/study/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.example.study.controller;
+
+import com.example.study.dto.MemberRequestDto;
+import com.example.study.dto.MemberResponseDto;
+import com.example.study.dto.TokenDto;
+import com.example.study.dto.TokenRequestDto;
+import com.example.study.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<MemberResponseDto> signup(@RequestBody MemberRequestDto memberRequestDto) {
+        return ResponseEntity.ok(authService.signup(memberRequestDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto memberRequestDto) {
+        return ResponseEntity.ok(authService.login(memberRequestDto));
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
+        return ResponseEntity.ok(authService.reissue(tokenRequestDto));
+    }
+}
